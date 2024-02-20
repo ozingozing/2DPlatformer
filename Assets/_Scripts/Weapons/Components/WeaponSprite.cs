ce9e3bda732093edc6236195b7aa2eb7101b6992
ozingozing <hoon4367@gmail.com>
2024-02-21 05:07:41 +0900

    [ADD] Test Multi Weapon

---
+++ Assets/_Scripts/Weapons/Components/WeaponSprite.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Ozing.Assets._Scripts.Weapons.Components
+{
+	public class WeaponSprite : WeaponComponent
+	{
+		private SpriteRenderer baseSpriteRender;
+		private SpriteRenderer weaponSpriteRender;
+
+		[SerializeField] private WeaponSprites[] weaponSprites;
+
+		private int currentWeaponSpriteIndex;
+
+		protected override void HandleEnter()
+		{
+			base.HandleEnter();
+			currentWeaponSpriteIndex = 0;
+		}
+
+		private void HandleBaseSpriteChange(SpriteRenderer sr)
+		{
+			if (!isAttackActive)
+			{
+				weaponSpriteRender.sprite = null;
+				return;
+			}
+			if(currentWeaponSpriteIndex < weaponSprites[weapon.CurrentAttackCounter].Sprites.Length)
+			{
+				weaponSpriteRender.sprite = weaponSprites[weapon.CurrentAttackCounter].Sprites[currentWeaponSpriteIndex];
+
+				currentWeaponSpriteIndex++;
+			}
+			else
+			{
+				Debug.LogWarning($"{weapon.name} weapon sprites length mismatch");
+				return;
+			}
+		}
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			baseSpriteRender = transform.Find("Base").GetComponent<SpriteRenderer>();
+			weaponSpriteRender = transform.Find("WeaponSprite").GetComponent<SpriteRenderer>();
+
+			// TODO: Fix this when we create weapon data
+			//baseSpriteRender = weapon.BaseGO.GetComponent<SpriteRenderer>();
+			//weaponSpriteRender = weapon.WeaponSpriteGO.GetComponent<SpriteRenderer>();
+		}
+
+		//유니티에도 이렇게 event함수를 사용하도록 권장하는 걸 보니깐 사용할 때 연결하고 사용이 끝나면 끊어버리네
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			baseSpriteRender.RegisterSpriteChangeCallback(HandleBaseSpriteChange);
+			weapon.OnEnter += HandleEnter;
+		}
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			baseSpriteRender.UnregisterSpriteChangeCallback(HandleBaseSpriteChange);
+			weapon.OnEnter -= HandleEnter;
+		}
+	}
+
+
+	[Serializable]
+	public class WeaponSprites
+	{
+		[field: SerializeField]public Sprite[] Sprites { get; private set; }
+	}
+}
--- Assets/_Scripts/Weapons/Weapon.cs
+++ Assets/_Scripts/Weapons/Weapon.cs
@@ -1,48 +1,84 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using Ozing.Assets._Scripts._ChocoOzing.Utilities;
 
 namespace Ozing.Assets._Scripts.Weapons
 {
 	public class Weapon : MonoBehaviour
 	{
+		[SerializeField] private int numberOfAttacks;
+		[SerializeField] private float attackCounterResetCooldown;
+		
+		public int CurrentAttackCounter
+		{
+			get => currentAttackCounter;
+			private set => currentAttackCounter = value >= numberOfAttacks ? 0 : value; 
+		}
+		private int currentAttackCounter;
+
+		public event Action OnEnter;
 		public event Action OnExit;
 
 		private Animator anim;
-		private GameObject baseGO;
+		public GameObject BaseGO { get; private set; }
+		public GameObject WeaponSpriteGO { get; private set; }
 
 		private AnimationEventHandler eventHandler;
+
+		private Timer attackCounterResetTimer;
 
 		public void Enter()
 		{
 			print($"{transform.name} enter");
 
+			attackCounterResetTimer.StopTimer();
+
 			anim.SetBool("active", true);
+			anim.SetInteger("counter", CurrentAttackCounter);
+
+			OnEnter?.Invoke();
 		}
 
 		private void Exit()
 		{
 			anim.SetBool("active", false);
+			
+
+			CurrentAttackCounter++;
+			attackCounterResetTimer.StartTimer();
 			//Onexit에 연결되어있는 PlayerAttackState클래스의 ExitHandler함수를 실행시켜주기 위함
 			OnExit?.Invoke();
 		}
 
+		private void Update()
+		{
+			attackCounterResetTimer.Tick();
+		}
+
 		private void Awake()
 		{
-			baseGO = transform.Find("Base").gameObject;
-			anim = baseGO.GetComponent<Animator>();
+			BaseGO = transform.Find("Base").gameObject;
+			WeaponSpriteGO = transform.Find("WeaponSprite").gameObject;
+			anim = BaseGO.GetComponent<Animator>();
 
-			eventHandler = baseGO.GetComponent<AnimationEventHandler>();
+			eventHandler = BaseGO.GetComponent<AnimationEventHandler>();
+
+			attackCounterResetTimer = new Timer(attackCounterResetCooldown);
 		}
+
+		private void ResetAttackCounter() => CurrentAttackCounter = 0;
 
 		private void OnEnable()
 		{
 			eventHandler.OnFinish += Exit;
+			attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
 		}
 
 		private void OnDisable()
 		{
 			eventHandler.OnFinish -= Exit;
+			attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
 		}
 	}
 }
